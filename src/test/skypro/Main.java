72a1596b33abb2037ac652bcc350f34ada247b88
student <i.i.f@mail.ru>
2022-03-31 18:44:35 +0800

    задание 1

--- src/test/skypro/Main.java
+++ src/test/skypro/Main.java
@@ -21,24 +21,5 @@ public class Main {
 
         book3.setYearOfPublishing(2002);
         System.out.println("book3 (переиздание) : " + book3);
-
-        System.out.println();
-        Book book4 = new Book ("Руслан и Людмила", author1, 2000);
-        if (book4.equals(book3)) System.out.println("book3 и book4 одинаковые");
-        else System.out.println("book3 и book4 разные");
-        System.out.println("book3 : " + book3);
-        System.out.println("book4 : " + book4);
-
-        System.out.println();
-        if (book2.equals(book1)) System.out.println("book1 и book2 одинаковые");
-        else System.out.println("book1 и book2 разные");
-        System.out.println("book1 : " + book1);
-        System.out.println("book2 : " + book2);
-
-        System.out.println();
-        if (author1.equals(author2)) System.out.println("author1 и author2 одинаковые");
-        else System.out.println("author1 и author2 разные");
-        System.out.println("author1 : " + author1);
-        System.out.println("author2 : " + author2);
     }
 }
